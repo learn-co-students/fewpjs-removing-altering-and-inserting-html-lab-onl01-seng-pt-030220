7fa5f35266ca19527c2df6a815f8d897f9379bac
MMcClure11 <mcclme02@luther.edu>
2020-08-19 11:59:27 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+
+let main = document.querySelector("main#main")
+main.remove();
+
+let newHeader = document.createElement('h1')
+document.body.appendChild(newHeader)
+newHeader.id = 'victory'
+newHeader.innerHTML = 'Meg is the champion'
