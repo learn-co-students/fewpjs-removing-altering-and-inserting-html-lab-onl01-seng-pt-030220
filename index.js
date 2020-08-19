142f527b304ac6c70fb1ecfe30e37a5a2f39ac01
SincerelyBrittany <sincerelybr@gmail.com>
2020-08-19 15:04:45 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+let element = document.querySelector("main#main")
+element.remove()
+
+const newHeader = document.createElement('h1')
+document.body.appendChild(newHeader)
+newHeader.id = "victory"
+
+// let newHeader = document.querySelector("h1#victory");
+newHeader.innerHTML = 'YOUR-NAME is the champion'
