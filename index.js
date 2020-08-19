03b9d24bf97465d82c5913421d71913670772fc4
jordles113 <jordan.t.romero@gmail.com>
2020-08-19 15:49:38 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+
+let main = document.querySelector('main#main')
+
+main.remove()
+
+let newHeader = document.createElement('h1')
+document.body.appendChild(newHeader)
+newHeader.id = "victory"
+newHeader.innerHTML = 'Jordles is the champion'
