cb8a607a3c80e1263bed278c51d8247265ba01ee
MMcClure11 <mcclme02@luther.edu>
2020-08-19 12:03:13 -0500

    Done.

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 // Write your code here!
 
-let main = document.querySelector("main#main")
-main.remove();
+// let main = document.querySelector("main#main")
+// main.remove();
 
-let newHeader = document.createElement('h1')
-document.body.appendChild(newHeader)
-newHeader.id = 'victory'
-newHeader.innerHTML = 'Meg is the champion'
+//learn co solution
+document.querySelector("main#main").remove();
+
+// let newHeader = document.createElement('h1')
+// document.body.appendChild(newHeader)
+// newHeader.id = 'victory'
+// newHeader.innerHTML = 'Meg is the champion'
+
+//learn co solution
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.innerHTML = "Flatiron School is the champion!";
+// newHeader.className = "victory";
