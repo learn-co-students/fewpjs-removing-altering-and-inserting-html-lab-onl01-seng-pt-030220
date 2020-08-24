c2ff976265d3b8e37bfa37f0fc1c2762256efd84
AHMED AFANDY <eng.computer1@yahoo.com>
2020-08-24 03:09:15 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+
+var element = document.querySelector("main")
+element.remove(element)
+
+
+var newHeader =document.createElement("H1")
+newHeader.setAttribute("id","victory")
+newHeader.innerHTML='YOUR-NAME is the champion'
