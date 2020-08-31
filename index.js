375ae6f3998025bda2d142d26cb019cad696d1d1
christian_fredericksen <christian.fredericksen@yahoo.com>
2020-08-31 16:39:44 -0700

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 // Write your code here! 
-//main.classList.remove("main");
-document.querySelector('body');
-body.removeChild('main')
+//document.querySelector('main#main').remove();
+// const newHeader = document.createElement('newHeader')
+// let victory = document.createElement('h1');
+// newHeader.appendChild(victory);
+// body.appendChild(newHeader);
+main.remove()
+let newHeader = document.createElement('h1')
+newHeader.setAttribute("id", "victory")
+newHeader.innerHTML = `#{Christian} is the champion`
