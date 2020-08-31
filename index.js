f878ebb9e149f79456bb6a356cb3834d21f2a098
Ahmed <wolktheline@yahoo.com>
2020-08-31 16:26:20 -0600

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
-// Write your code here!
-const testVar = "hi"
+// // Write your code here!
+// const testVar = "hi"
 
-/* Enter the code to remove the node element here */
-document.querySelector("main#main").remove();
+// /* Enter the code to remove the node element here */
+// document.querySelector("main#main").remove();
 
-/* Create your new element here and assign it to newHeader */
-const newHeader = document.createElement("h1");
-newHeader.id = "victory"
-newHeader.innerHTML = "Ahmed is the champion!";
-newHeader.className = "victory";
+// /* Create your new element here and assign it to newHeader */
+// const newHeader = document.createElement("h1");
+// newHeader.id = "victory"
+// newHeader.innerHTML = "Ahmed is the champion!";
+// newHeader.className = "victory";
+
+main.remove()
+let newHeader = document.createElement('h1')
+newHeader.setAttribute("id", "victory")
+newHeader.innerHTML = `#{your_name} is the champion`
+
